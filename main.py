98e238deed91b6ed1fd42edc53223cc4909d6d1f
burlinova <130795074+burlinova@users.noreply.github.com>
2023-11-25 10:27:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import requests
 import telebot
-from io import BytesIO #импортируем метод io з библиотеки для чего??
+from io import BytesIO 
 import random
 
-bot = telebot.TeleBot("6709487661:AAHVQBLeuhQ8AXLFhS_5KgZPGYHneKgdh0M") # сюда вставляете ваш токен бота
+bot = telebot.TeleBot("6709487661:AAHVQBLeuhQ8AXLFhS_5KgZPGYHneKgdh0M") 
 
 channel_id = 428515560
 
 
 
 def meme(channel_id):
-    client_id = 'wa6mcWkE2xQLi0PCLIzZfg' #взяли с сайта это что то про моего бота?
+    client_id = 'wa6mcWkE2xQLi0PCLIzZfg' 
     channel_id = channel_id
-    client_secret = 'rg6DVa-9C8vgk5pLbDx3eOZPRPryCw'  # данные для аутентификации на reddit
+    client_secret = 'rg6DVa-9C8vgk5pLbDx3eOZPRPryCw' 
     user_agent = 'IlichSameAsBobAgent (by /u/Hopeful_Hearing_3354)'
 
     url = 'https://www.reddit.com/r/memes/new.json?limit=100'
@@ -25,18 +25,18 @@ def meme(channel_id):
     if response.status_code == 200:
         print("Ответ с кучей картинок получен, пытаемся получить картинку")
         data = response.json()
-        posts = data['data']['children'] #&
+        posts = data['data']['children'] 
 
         random_post = random.choice(posts)
-        image_url = random_post['data']['url'] #?
+        image_url = random_post['data']['url'] 
 
         if any(extension in image_url.lower() for extension in
-               ['.jpg', '.jpeg', '.png', '.gif']):  # Проверяем, что URL ведет на изображение
+               ['.jpg', '.jpeg', '.png', '.gif']): 
             print(f"Ссылка на картинку: {image_url}")
             image_response = requests.get(image_url, headers=headers, auth=auth)
             print("Получили картинку!")
             image_data = BytesIO(
-                image_response.content)  # Используем буфер, чтобы потом "прочитать" из него картинку и отправить в бота
+                image_response.content) 
             bot.send_photo(chat_id=channel_id, photo=image_data)
             print("Картинка отправлена в бота")
             return True
